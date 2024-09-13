d1d726e0f19ec061e52dc555e6cbe9a711904723
kunal004qwerty <109149822+kunal004qwerty@users.noreply.github.com>
2024-09-13 16:29:29 +0530

    load planet data in test

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -5,12 +5,14 @@ const {
     mongoConnect,
     mongoDisconnect,
 } = require('../../services/mongo')
+const { loadPlanetData } = require('../../models/planets.model')
 
 
 describe('Launches API', () => {
 
     beforeAll(async () => {
         await mongoConnect()
+        await loadPlanetData()
     })
 
     afterAll(async () => {
